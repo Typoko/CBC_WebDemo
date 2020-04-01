665ea9fffe091b1886d662f33e5dc2d555817c37
Timo Raivonen <timoraivonen@gmail.com>
2020-04-01 10:28:23 +0300

    Adding ASP.NET base to git.

---
+++ ASPNETWebDemo/ASPNETWebDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETWebDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CBC_WebDemo
 Demonstration of being able to code. Made during Code Boot Camp.
+
+Käytetyt tekniikat:
+ - C#
