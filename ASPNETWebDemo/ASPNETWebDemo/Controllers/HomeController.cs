734bc323425f68049bc9a47b9714e5b60c6fa3cd
Timo Raivonen <timoraivonen@gmail.com>
2020-04-05 14:07:42 +0300

    Added test image, reference to ImageStream and initial testing of pixel handling of the image.

--- ASPNETWebDemo/ASPNETWebDemo/Controllers/HomeController.cs
+++ ASPNETWebDemo/ASPNETWebDemo/Controllers/HomeController.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ASPNETWebDemo.Models;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ASPNETWebDemo.Controllers
 {
@@ -17,7 +23,7 @@ namespace ASPNETWebDemo.Controllers
         {
             _logger = logger;
         }
-
+        
         public IActionResult Index()
         {
             return View();
@@ -33,5 +39,64 @@ namespace ASPNETWebDemo.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public string TestiString()
+        {
+            return "Testi 123";
+        }
+
+        public string Kellonaika()
+        {
+            return DateTime.Now.ToString();
+        }
+
+
+        public IActionResult UusiSivu()
+        {
+            List<Asiakas> asiakkaat = new List<Asiakas>()
+            {
+                new Asiakas()
+                {
+                    AsiakasID = 100,
+                    asiakkaanNimi = "Nimi1",
+                    sahkoPosti = "testi@123.fi"
+                },
+                new Asiakas()
+                {
+                    AsiakasID = 101,
+                    asiakkaanNimi = "Nimi2",
+                    sahkoPosti = "te2222sti@123.fi"
+                }
+            };
+
+            ViewBag.NapinVari = "danger";
+            ViewBag.NaytaLista = "false";
+            
+            return View(asiakkaat);
+        }
+
+        private readonly IWebHostEnvironment _env;
+
+        public IActionResult KartanMuunnos()
+        {
+            ViewBag.KarttaPath = "/TestFolder/testiKartta.jpg";
+            //var karttaLadattu = Image<Rgba32>.Load(_env.WebRootPath + "/testiKartta.jpg");
+            Image<Rgba32> karttaLadattu = Image.Load<Rgba32>("F:/Git_Repo/CBC_WebDemo/ASPNETWebDemo/ASPNETWebDemo/wwwroot/TestFolder/testiKartta.jpg");
+
+
+            //karttaLadattu[1, 1] = Rgba32.Red;
+
+            //var kartta = new Image<Rgba32>(400, 400);
+
+            ViewBag.Karttatesti = "fail";
+            karttaLadattu[1, 1] = Rgba32.Red;
+
+            if (karttaLadattu[1, 1] == Rgba32.Red)
+                ViewBag.Karttatesti = "success";
+
+            return View();
+        }
+
+
     }
 }
