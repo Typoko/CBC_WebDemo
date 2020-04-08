371ac7135309681203b4bdb3fdebf544a07f47ab
Timo Raivonen <timoraivonen@gmail.com>
2020-04-08 11:09:41 +0300

    Hämmentävä virhe havaittu, pitää refreshaa selain, että kuvat latautuu. Oletan, että johtuu purkkakoodista. Lisätty skaalaavuus kuvan koolle. Kartta luokka saanut lisää muuttujia kuvan oikeaan asetteluun ja controller ja sivu muokattu käyttämään muuttujia.

--- ASPNETWebDemo/ASPNETWebDemo/Controllers/HomeController.cs
+++ ASPNETWebDemo/ASPNETWebDemo/Controllers/HomeController.cs
@@ -79,34 +79,43 @@ namespace ASPNETWebDemo.Controllers
 
         public IActionResult KartanMuunnos()
         {
-            ViewBag.KarttaPath = "/TestFolder/testiKartta.jpg";
+            ViewBag.KarttaPath = "/TestFolder/testiKartta2.jpg";
             //var karttaLadattu = Image<Rgba32>.Load(_env.WebRootPath + "/testiKartta.jpg");
-            Image<Rgba32> karttaLadattu = Image.Load<Rgba32>("F:/Git_Repo/CBC_WebDemo/ASPNETWebDemo/ASPNETWebDemo/wwwroot/TestFolder/testiKartta.jpg");
-            Ruutu[,] RuutuStatusTable = new Ruutu[50, 50];
+            Kartta kartta = new Kartta();
+            kartta.KarttaKuva = Image.Load<Rgba32>("F:/Git_Repo/CBC_WebDemo/ASPNETWebDemo/ASPNETWebDemo/wwwroot/TestFolder/testiKartta2.jpg");
+            
+            kartta.RuutuTable = new Ruutu[(kartta.KarttaKuva.Width)/20, kartta.KarttaKuva.Height/20];
+
+            //Image<Rgba32> karttaLadattu = Image.Load<Rgba32>("F:/Git_Repo/CBC_WebDemo/ASPNETWebDemo/ASPNETWebDemo/wwwroot/TestFolder/testiKartta.jpg");
+            //Ruutu[,] RuutuStatusTable = new Ruutu[50, 50];
             //bool RuutuTyyppi = false;
 
             //En ymmärrä miksi, mutta new Ruutu[50,50]; ei täytä taulukkoa uusilla instansseilla.
             //Funktio täyttää taulun uusilla sen sijaan.
-            SetRuutuInstances(RuutuStatusTable);
+            SetRuutuInstances(kartta.RuutuTable);
 
-            SetRuutuHuoneStatus(RuutuStatusTable, karttaLadattu);
+            SetRuutuHuoneStatus(kartta.RuutuTable, kartta.KarttaKuva);
 
-            SetRuutuSeinaStatus(RuutuStatusTable);
+            SetRuutuSeinaStatus(kartta.RuutuTable);
 
-            SetRuutuKulmaStatus(RuutuStatusTable);
+            SetRuutuKulmaStatus(kartta.RuutuTable);
 
+            SetKuvaMinMaxRuudut(kartta);
 
+            //kartta.TulosteX = 1000;
+            //kartta.TulosteY = 1000;
+            
             //karttaLadattu[1, 1] = Rgba32.Red;
 
             //var kartta = new Image<Rgba32>(400, 400);
 
-            ViewBag.Karttatesti = "fail";
-            karttaLadattu[1, 1] = Rgba32.Red;
+            //ViewBag.Karttatesti = "fail";
+            //karttaLadattu[1, 1] = Rgba32.Red;
 
-            if (karttaLadattu[1, 1] == Rgba32.Red)
-                ViewBag.Karttatesti = "success";
+            //if (karttaLadattu[1, 1] == Rgba32.Red)
+            //    ViewBag.Karttatesti = "success";
 
-            return View(RuutuStatusTable);
+            return View(kartta);
         }
 
         public bool OnkoSeinaValissa(int x, int y, Image<Rgba32> kuva)
@@ -126,10 +135,10 @@ namespace ASPNETWebDemo.Controllers
         {
             bool RuutuTyyppi = false;
 
-            for (int i = 0; i < 49; i++)
+            for (int i = 0; i < ruudut.GetLength(0)-1; i++)
             {
                 RuutuTyyppi = false;
-                for (int j = 0; j < 49; j++)
+                for (int j = 0; j < ruudut.GetLength(1)-1; j++)
                 {
                     ruudut[i, j].OnkoAvoin = RuutuTyyppi;
                     if (OnkoSeinaValissa(i, j, kuva))
@@ -143,9 +152,9 @@ namespace ASPNETWebDemo.Controllers
 
         public void SetRuutuInstances(Ruutu[,] ruudut)
         {
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < ruudut.GetLength(0); i++)
             {
-                for (int j = 0; j < 50; j++)
+                for (int j = 0; j < ruudut.GetLength(1); j++)
                 {
                     ruudut[i, j] = new Ruutu();
                 }
@@ -157,9 +166,9 @@ namespace ASPNETWebDemo.Controllers
         {
             //käydään läpi keskiruudut ja reunaruudut erikseen, ettei tule out of bounds viittauksia
             //vältytään joka ruudun kohdalla out of bounds testauksen tarpeesta
-            for (int i = 1; i < 49; i++)
+            for (int i = 1; i < ruudut.GetLength(0)-1; i++)
             {
-                for (int j = 1; j < 49; j++)
+                for (int j = 1; j < ruudut.GetLength(1)-1; j++)
                 {
                     if(ruudut[i,j].OnkoAvoin == false)
                     {
@@ -201,31 +210,31 @@ namespace ASPNETWebDemo.Controllers
                     }
                 }
                 //Erikseen tarkastettavat reunat
-                if (ruudut[i, 1].OnkoAvoin == true)
-                {
-                    ruudut[i, 0].Seinat[2] = true;
-                }
-                if (ruudut[48, i].OnkoAvoin == true)
-                {
-                    ruudut[49, i].Seinat[3] = true;
-                }
-                if (ruudut[i, 48].OnkoAvoin == true)
-                {
-                    ruudut[i, 49].Seinat[0] = true;
-                }
-                if (ruudut[1, i].OnkoAvoin == true)
-                {
-                    ruudut[0, i].Seinat[1] = true;
-                }
+                //if (ruudut[i, 1].OnkoAvoin == true)
+                //{
+                //    ruudut[i, 0].Seinat[2] = true;
+                //}
+                //if (ruudut[ruudut.GetLength(0)-2, i].OnkoAvoin == true)
+                //{
+                //    ruudut[ruudut.GetLength(0)-1, i].Seinat[3] = true;
+                //}
+                //if (ruudut[i, ruudut.GetLength(1)-2].OnkoAvoin == true)
+                //{
+                //    ruudut[i, ruudut.GetLength(1)-1].Seinat[0] = true;
+                //}
+                //if (ruudut[1, i].OnkoAvoin == true)
+                //{
+                //    ruudut[0, i].Seinat[1] = true;
+                //}
             }
         }
 
         public void SetRuutuKulmaStatus(Ruutu[,] ruudut)
         {
             bool ruutuStatus = false;
-            for (int i = 1; i < 49; i++)
+            for (int i = 1; i < ruudut.GetLength(0)-1; i++)
             {
-                for (int j = 1; j < 49; j++)
+                for (int j = 1; j < ruudut.GetLength(1)-1; j++)
                 {
                     ruutuStatus = ruudut[i, j].OnkoAvoin;
 
@@ -267,5 +276,45 @@ namespace ASPNETWebDemo.Controllers
             }
         }
 
+        public void SetKuvaMinMaxRuudut(Kartta kKartta)
+        {
+            kKartta.MinX = kKartta.RuutuTable.GetLength(0)-1;
+            kKartta.MinY = kKartta.RuutuTable.GetLength(1)-1;
+            kKartta.MaxX = 0;
+            kKartta.MaxY = 0;
+            for (int i = 0; i < kKartta.RuutuTable.GetLength(0); i++)
+            {
+                for (int j = 0; j < kKartta.RuutuTable.GetLength(1); j++)
+                {
+                    if (kKartta.RuutuTable[i, j].OnkoAvoin)
+                    {
+                        if (kKartta.MinX>i)
+                        {
+                            kKartta.MinX = i;
+                        }
+                        if (kKartta.MinY>j)
+                        {
+                            kKartta.MinY = j;
+                        }
+                        if (kKartta.MaxX<i)
+                        {
+                            kKartta.MaxX = i;
+                        }
+                        if (kKartta.MaxY<j)
+                        {
+                            kKartta.MaxY = j;
+                        }
+                    }
+                }
+            }
+            kKartta.MinX--;
+            kKartta.MinY--;
+            kKartta.MaxX++;
+            kKartta.MaxY++;
+
+            kKartta.TulosteX = (kKartta.MaxX - kKartta.MinX + 1) * 20;
+            kKartta.TulosteY = (kKartta.MaxY - kKartta.MinY + 1) * 20;
+
+        }
     }
 }
---
+++ ASPNETWebDemo/ASPNETWebDemo/Models/Kartta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace ASPNETWebDemo.Models
+{
+
+    public class Kartta
+    {
+        public Image<Rgba32> KarttaKuva;// = Image.Load<Rgba32>("F:/Git_Repo/CBC_WebDemo/ASPNETWebDemo/ASPNETWebDemo/wwwroot/TestFolder/testiKartta.jpg");
+        public Ruutu[,] RuutuTable;// = new Ruutu[50, 50];
+        public int MinX = 0;
+        public int MinY = 0;
+        public int MaxX = 0;
+        public int MaxY = 0;
+        public int TulosteX = 0;
+        public int TulosteY = 0;
+    }
+}
